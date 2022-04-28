575b3bdde53199f395bdfa3419deaf86e359fa87
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-04-28 19:40:32 +0200

    Update Open Source Backrooms Entity ESP.lua

--- Open Source Backrooms Entity ESP.lua
+++ Open Source Backrooms Entity ESP.lua
@@ -1,53 +1,33 @@
 --credits to Kiriot22 for the esp lib.
 
+local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/BoredStuff2/notify-lib/main/lib'),true))()
+
+Lib.prompt('WARNING', "Entity ESP is slightly buggy, re-execute script if you can't see the entities.", 15)
+Lib.prompt('INFO', "You should see 2 entities.", 10)
+
+
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiriot22/ESP-Lib/main/ESP.lua"))()
 
-local entity1exists = false
-local entity2exists = false
-
-
 local alreadyexisting = {
-    Name = "ENTITY",
     Color = Color3.new(255, 0, 0),
+    Name = "ENTITY"
 }
-
-
-local entity1 = game:GetService("Workspace"):FindFirstChild("ENTITY_1")
-
-if not entity1 then entity1exists = false
-    else
-entity1exists = true
-end
-local entity2 = game:GetService("Workspace"):FindFirstChild("ENTITY_DANCE")
-
-if not entity2 then entity2exists = false
-    else
-entity1exists = true
-end
 
 if game.PlaceId == 8926741973 then
 
-if entity1exists == true then
-ESP:Add(workspace.ENTITY_1, alreadyexisting)
-else
 ESP:AddObjectListener(workspace, {
     Color = Color3.new(255, 0, 0),
     Type = "Model",
     Name = "ENTITY_1",
     CustomName = "ENTITY"
 })
-end
 
-if entity2exists == true then
-ESP:Add(workspace.ENTITY_DANCE, alreadyexisting)
-else
 ESP:AddObjectListener(workspace, {
     Color = Color3.new(255, 0, 0),
     Type = "Model",
     Name = "ENTITY_DANCE",
     CustomName = "ENTITY"
 })
-end
 
 ESP:AddObjectListener(workspace, {
     Color = Color3.new(0, 255, 255),
@@ -55,6 +35,9 @@ ESP:AddObjectListener(workspace, {
     Name = "door01_left_Body",
     CustomName = "Door"
 })
+
+ESP:Add(workspace.ENTITY_1, alreadyexisting)
+ESP:Add(workspace.ENTITY_DANCE, alreadyexisting)
 
 elseif game.PlaceId == 9204234205 then
     
@@ -67,4 +50,5 @@ ESP:AddObjectListener(workspace, {
 })
 
 end
+ESP.Players = false
 ESP:Toggle(true)
