14024509c5db9c512937c10559cf6a28fecc98c8
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-05-17 17:39:52 +0200

    update to add all of the functions i have

--- funny function thing.lua
+++ funny function thing.lua
@@ -1,7 +1,18 @@
 cg = game.PlaceId
 
+hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+
 function hget(filepath)
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/floppamod-getmodule/main/"..filepath))()
+    return loadstring("https://raw.githubusercontent.com/fheahdythdr/floppamod-getmodule/main/"..filepath)
+end
+
+function tablech(table, item, valuen)
+    if table.find(table, item) then
+        valuen = true
+    else
+        valuen = false
+return valuen
+end
 end
 
 function  tload(tab, item, loader)
@@ -10,4 +21,26 @@ function  tload(tab, item, loader)
     else
         return print("wrong game lol")
     end
+end
+
+function ccfrs(cframe, genv)
+    if hrp then
+        genv = hrp.CFrame
+        hrp.CFrame = cframe or CFrame.new(cframe) or cframe.CFrame
+    end
+    return print(genv)
+end
+
+function rtop(genv)
+    if genv then
+        hrp.CFrame = genv or CFrame.new(genv)
+    end
+end
+
+function valch(value, func)
+    if value == true then
+        return func
+    else
+        return print(""..value "is not true.")
+end
 end
