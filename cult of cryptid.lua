3bee250b41f6a318c3d4d29265eb45a9c185bfd0
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-05-17 18:53:16 +0200

    Update cult of cryptid.lua

--- cult of cryptid.lua
+++ cult of cryptid.lua
@@ -2,11 +2,9 @@ function hget(filepath)
     return loadstring(game:HttpGet("https://raw.githubusercontent.com/fheahdythdr/floppamod-getmodule/main/"..filepath))()
 end
 
-function  tload(tab, item, loader)
+function tload(tab, item, loader)
     if table.find(tab, item) then
         return hget(loader)
-    else
-        return print("wrong game lol")
     end
 end
 
@@ -14,5 +12,6 @@ cg = game.PlaceId
 
 hget("tablefortload")
 
+tload(cotclobby, cg, "cotc_lobby")
 tload(chapter1, cg, "cotc_1")
 tload(chapter2, cg, "cotc_2")
