646f63302c4de59d10159ca8f772afdd6c587983
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-05-17 16:03:18 +0200

    add tload function

--- funny function thing.lua
+++ funny function thing.lua
@@ -1,3 +1,11 @@
 function hget(filepath)
     return loadstring("https://raw.githubusercontent.com/fheahdythdr/floppamod-getmodule/main/"..filepath)
 end
+
+function  tload(table, item, loader)
+    if table.find(table, item) then
+        return hget(loader)
+    else
+        return print("wrong game lol")
+    end
+end
